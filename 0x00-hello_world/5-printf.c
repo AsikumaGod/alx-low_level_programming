cdeb0654c1cfdc2a5d918bf13a59bdd3af304a8d
AsikumaGod <jacobantoine506@gmail.com>
2023-02-16 08:33:28 -0800

    hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints with proper grammar, but the outcome is a piecse of art,
+ *	followed by a new line 
+ *
+ *	Return: Always zero
+ */
+
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
