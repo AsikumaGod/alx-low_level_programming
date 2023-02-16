65538701d96af317aa15340bbc7bee815889e1ed
AsikumaGod <jacobantoine506@gmail.com>
2023-02-16 08:01:29 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-	puts("\"Programming is just like building a multilingual puzzle");
-return 0;
+	puts("\"Programming is just building a multilingual puzzle");
+	return (0);
 }
