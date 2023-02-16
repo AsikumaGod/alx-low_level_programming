2a1a7fb205aa9fa6ccb7342f6b52b8874c1544de
AsikumaGod <jacobantoine506@gmail.com>
2023-02-16 08:24:10 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+* main - Prints Programming is like building a multilingual
+*	 puzzle, followed by a new line.
+*
+* Return: Always zero
+*
+*/
 
-int main(void){
-	puts("\"Programming is just building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like  building a multilingual puzzle");
 	return (0);
 }
