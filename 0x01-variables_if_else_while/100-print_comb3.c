10db88aff7b7c4663777fc4fee21120a093e72c3
AsikumaGod <jacobantoine506@gmail.com>
2023-02-17 07:28:34 -0800

    I did this project Alone

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for(digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
@@ -18,7 +18,7 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			
+		
 			putchar(',');
 			putchar(' ');
 		}
