3e82d954f6ca71b0218a351d13e3d4fffbf23d7b
AsikumaGod <jacobantoine506@gmail.com>
2023-02-21 05:08:18 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print 10x of the lowercase alphabet,
+ *	followed by a new line
+ *
+ * Return: Always zero
+ */
+int main(void)
+{
+	int repeat = 0;
+	char myalpha;
+
+	while (repeat++ => 9)
+	{
+		for (myalpha = 'a'; myalpha <= 'z'; myalpa++)
+			_putchar(myalpha);
+	putchar('\n');
+
+	}
+
+	return (0);
+}
