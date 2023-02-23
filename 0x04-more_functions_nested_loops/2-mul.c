03ea095da931e1e467485a4378d6be4786ecf6cd
AsikumaGod <jacobantoine506@gmail.com>
2023-02-23 04:11:59 -0800

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two digits
+ *@y: the first number to multiply
+ *@p: the second number to multiply
+ *
+ *Return: output the result of the multiplication
+ */
+int mul(int y, int p)
+{
+	return(y * p);
+}
